02d31136eb57fc24ee79823df3c639efb7062b3d
rafalkfb <rafal.kfb@gmail.com>
2022-02-14 12:47:03 +0100

    setting language for headless option

--- tests_cases/courses/register_courses_multiple.py
+++ tests_cases/courses/register_courses_multiple.py
@@ -1,12 +1,14 @@
-import time
-
 import pytest
+from selenium.webdriver.common.by import By
+
 from pages.courses.register_courses_page import RegisterCoursesPage
 import unittest
 from utilities.checkteststatus import CheckTestStatus
+from ddt import ddt, data, unpack
 
 
 @pytest.mark.usefixtures("one_time_set_up", "set_up")
+@ddt
 class RegisterCoursesTests(unittest.TestCase):
 
     @pytest.fixture(autouse=True)
@@ -18,11 +20,13 @@ class RegisterCoursesTests(unittest.TestCase):
         self.courses = RegisterCoursesPage(self.driver)
 
     @pytest.mark.run(order=1)
-    def test_course_page_enter(self):
-        self.courses.enter_course_name("JavaScript")
-        self.courses.select_course_to_enroll("JavaScript for beginners")
-        self.courses.enroll_course(5168284143633642, 1224, 324)
-        time.sleep(2)
-
+    @data(("JavaScript for beginners", 5168284143633642, 1224, 324))
+    @unpack
+    def test_course_page_enter(self, course_name, cc_num, cc_exp, cc_cvv):
+        self.courses.enter_course_name(course_name)
+        self.courses.select_course_to_enroll(course_name)
+        self.courses.enroll_course(cc_num, cc_exp, cc_cvv)
         result = self.courses.verify_enroll_failed()
         self.test_status.mark_final("is error displayed", result, "error is not displayed")
+        self.courses.element_click("ALL COURSES", 'linktext')
+        # self.driver.find_element(By.LINK_TEXT, "All Courses").click()
