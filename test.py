53739981c835b30d564b13d4c0916fba872ea5ce
rafalkfb <rafal.kfb@gmail.com>
2022-02-25 09:37:15 +0100

    update in test.py for test connection from docker

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ from selenium import webdriver
 def main():
     options = webdriver.ChromeOptions()
     options.set_capability("loggingPrefs", {'performance': 'ALL'})
-    driver = webdriver.Remote(command_executor="localhost:4444", options=options)
+    driver = webdriver.Remote(command_executor="http://selenium-hub:4444", options=options)
     driver.get("https://www.google.com")
     print(driver.title)
     driver.quit()
