3c03f69e6035bb499817a375e3084ee54ea14851
rafalkfb <rafal.kfb@gmail.com>
2022-03-07 17:05:02 +0100

    changed implicit wait variable to import from config class

--- base/selenium_driver.py
+++ base/selenium_driver.py
@@ -1,7 +1,7 @@
 import logging
 import time
 from traceback import print_stack
-
+from configfiles.config_data import Config
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as ec
@@ -48,13 +48,9 @@ class SeleniumDriver:
 
     def get_element(self, locator_tuple):
 
-        element = None
         by_type, locator = locator_tuple
-        try:
-            element = self.driver.find_element(by_type, locator)
-            self.log.info("Element found with locator: " + str(locator_tuple))
-        except NoSuchElementException:
-            self.log.error("Element not found with locator: " + str(locator_tuple))
+        element = self.driver.find_element(by_type, locator)
+        self.log.info("Element found with locator: " + str(locator_tuple))
         return element
 
     def get_element_list(self, locator_tuple):
@@ -63,13 +59,9 @@ class SeleniumDriver:
         :param locator_tuple:
         :return:
         """
-        element = None
         by_type, locator = locator_tuple
-        try:
-            element = self.driver.find_elements(by_type, locator)
-            self.log.info("Element list found with locator: " + str(locator_tuple))
-        except NoSuchElementException:
-            self.log.error("Element list not found with locator: " + str(locator_tuple))
+        element = self.driver.find_elements(by_type, locator)
+        self.log.info("Element list found with locator: " + str(locator_tuple))
         return element
 
     def element_click(self, locator_tuple=None, element=None):
@@ -80,14 +72,10 @@ class SeleniumDriver:
         :param element:
         :return:
         """
-        try:
-            if locator_tuple:
-                element = self.get_element(locator_tuple)
-            element.click()
-            self.log.info("Clicked on element with locator: " + str(locator_tuple))
-        except (NoSuchElementException, ElementNotVisibleException, ElementNotSelectableException):
-            self.log.error("Cannot click on element with locator: " + str(locator_tuple))
-            print_stack()
+        if locator_tuple:
+            element = self.get_element(locator_tuple)
+        element.click()
+        self.log.info("Clicked on element with locator: " + str(locator_tuple))
 
     def element_send_keys(self, data, locator_tuple=None, element=None):
         """
@@ -98,14 +86,10 @@ class SeleniumDriver:
         :param locator_tuple:
         :return:
         """
-        try:
-            if locator_tuple:
-                element = self.get_element(locator_tuple)
-            element.send_keys(data)
-            self.log.info("Send text on element with locator: " + str(locator_tuple))
-        except (NoSuchElementException, ElementNotVisibleException, ElementNotSelectableException):
-            self.log.error("Cannot send text on element with locator: " + str(locator_tuple))
-            print_stack()
+        if locator_tuple:
+            element = self.get_element(locator_tuple)
+        element.send_keys(data)
+        self.log.info("Send text on element with locator: " + str(locator_tuple))
 
     def get_text(self, locator_tuple=None, element=None):
         """
@@ -115,19 +99,15 @@ class SeleniumDriver:
         :param element:
         :return:
         """
-        try:
-            if locator_tuple:
-                element = self.get_element(locator_tuple)
-            text = element.text
-            if len(text) == 0:
-                text = element.get_attribute("innerText")
-            if len(text) != 0:
-                self.log.info("Getting text on element :: " + str(locator_tuple))
-                self.log.info("The text is :: '" + text + "'")
-                text = text.strip()
-        except:
-            self.log.error("Failed to get text on element " + str(locator_tuple))
-            text = None
+        if locator_tuple:
+            element = self.get_element(locator_tuple)
+        text = element.text
+        if len(text) == 0:
+            text = element.get_attribute("innerText")
+        if len(text) != 0:
+            self.log.info("Getting text on element :: " + str(locator_tuple))
+            self.log.info("The text is :: '" + text + "'")
+            text = text.strip()
         return text
 
     def is_element_present(self, locator_tuple=None, element=None):
@@ -144,7 +124,7 @@ class SeleniumDriver:
             if element is not None:
                 self.log.info("Element present with locator: " + str(locator_tuple))
                 return True
-        except NoSuchElementException:
+        except (NoSuchElementException, TimeoutException, ElementNotVisibleException):
             self.log.error("Element not present with locator: " + str(locator_tuple))
             return False
 
@@ -170,8 +150,7 @@ class SeleniumDriver:
             self.log.error("Element not found with locator: " + str(locator_tuple))
             return False
 
-    def element_present_check(self, locator_tuple):
-
+    def element_present_check_by_list(self, locator_tuple):
         by_type, locator = locator_tuple
         elements = self.driver.find_elements(by_type, locator)
         if len(elements) > 0:
@@ -182,25 +161,22 @@ class SeleniumDriver:
             return False
 
     def verify_title_contains(self, title: str):
-        try:
-            if title in self.get_title():
-                return True
-            else:
-                self.log.error("### Given title: " + title + "  does not contains in actual title: " + self.get_title())
-                return False
-        except:
-            self.log.error("### Failed to get page title")
+
+        if title in self.get_title():
+            self.log.info("### Given title: " + title + "  contains in actual title: " + self.get_title())
+            return True
+        else:
+            self.log.error("### Given title: " + title + "  does not contains in actual title: " + self.get_title())
             return False
 
     def verify_title_match(self, title: str):
-        try:
-            if title == self.get_title():
-                return True
-            else:
-                self.log.error("### Given title: " + title + "   does not match with actual title: " + self.get_title())
-                return False
-        except :
-            self.log.error("### Failed to get page title")
+
+        if title == self.get_title():
+            self.log.info("### Given title: " + title + "   match with actual title: " + self.get_title())
+            return True
+        else:
+            self.log.error("### Given title: " + title + "   does not match with actual title: " + self.get_title())
+            return False
 
     def scroll_down(self, how_much=-1000):
         """
@@ -231,21 +207,17 @@ class SeleniumDriver:
         :return:
         """
         element = self.get_element(locator_tuple)
-        enabled = False
-        try:
-            attribute_value = self.get_element_attribute_value(attribute="disabled", element=element)
-            if attribute_value is not None:
-                enabled = element.is_enabled()
-            else:
-                value = self.get_element_attribute_value(attribute="class", element=element)
-                self.log.info("Attribute value From Application Web UI :: " + value)
-                enabled = not ("disabled" in value)
-            if enabled:
-                self.log.info("Element :: '" + str(locator_tuple) + "' is enabled")
-            else:
-                self.log.info("Element :: '" + str(locator_tuple) + "' is not enabled")
-        except NoSuchElementException:
-            self.log.error("Element :: '" + str(locator_tuple) + "' state could not be found")
+        attribute_value = self.get_element_attribute_value(attribute="disabled", element=element)
+        if attribute_value is not None:
+            enabled = element.is_enabled()
+        else:
+            value = self.get_element_attribute_value(attribute="class", element=element)
+            self.log.info("Attribute value From Application Web UI :: " + value)
+            enabled = not ("disabled" in value)
+        if enabled:
+            self.log.info("Element :: '" + str(locator_tuple) + "' is enabled")
+        else:
+            self.log.info("Element :: '" + str(locator_tuple) + "' is not enabled")
         return enabled
 
     def switch_frame_by_index(self, locator_tuple):
@@ -255,36 +227,27 @@ class SeleniumDriver:
         :return:
         """
         result = False
-        try:
-            iframe_list = self.get_element_list((By.XPATH, "//iframe"))
-            if len(iframe_list) == 0:
-                self.log.warning("There are no iframes")
-            for i in range(len(iframe_list)):
-                self.driver.switch_to.frame(iframe_list[i])  # or i +1 ?
-                result = self.is_element_present(locator_tuple)
-                if result:
-                    self.log.info("iframe index is: " + str(iframe_list[i]))
-                    break
-                self.driver.switch_to.default_content()
-            return result
-        except:
-            self.log.error("iframe index not found")
-            return result
+        iframe_list = self.get_element_list((By.XPATH, "//iframe"))
+        if len(iframe_list) == 0:
+            self.log.warning("There are no iframes")
+        for i in range(len(iframe_list)):
+            self.driver.switch_to.frame(iframe_list[i])  # or i +1 ?
+            result = self.is_element_present(locator_tuple)
+            if result:
+                self.log.info("iframe index is: " + str(iframe_list[i]))
+                break
+            self.driver.switch_to.default_content()
+        return result
 
     def wait_for_element(self, locator_tuple, timeout=10, poll_frequency=0.5):
 
-        element = None
-        try:
-            self.driver.implicitly_wait(0)  # just in case you need to use this method, do not mix imp and exp ways
-            self.log.info("Waiting for maximum :: " + str(timeout) + " :: seconds for element to be visible")
-            wait = WebDriverWait(self.driver, timeout=timeout, poll_frequency=poll_frequency,
-                                 ignored_exceptions=[NoSuchElementException,
-                                                     ElementNotVisibleException,
-                                                     ElementNotSelectableException, ])
-            element = wait.until(ec.visibility_of_element_located(locator_tuple))
-            self.log.info("Element appeared on the web page")
-        except (NoSuchElementException, ElementNotVisibleException, ElementNotSelectableException, TimeoutException):
-            self.log.info("Element not appeared on the web page")
-            print_stack()
-        self.driver.implicitly_wait(10)  # set up to the default value
+        self.driver.implicitly_wait(0)  # just in case you need to use this method, do not mix imp and exp ways
+        self.log.info("Waiting for maximum :: " + str(timeout) + " :: seconds for element to be visible")
+        wait = WebDriverWait(self.driver, timeout=timeout, poll_frequency=poll_frequency,
+                             ignored_exceptions=[NoSuchElementException, ElementNotVisibleException,
+                                                 ElementNotSelectableException, ])
+        element = wait.until(ec.visibility_of_element_located(locator_tuple))
+        self.log.info("Element appeared on the web page")
+
+        self.driver.implicitly_wait(Config.IMPLICIT_WAIT)  # set up to the default value
         return element
