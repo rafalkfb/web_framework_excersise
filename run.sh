c10d87c09548980c0666b7c7cf64d724aac054ad
rafalkfb <rafal.kfb@gmail.com>
2022-04-30 15:29:52 +0200

    before excersise

--- run.sh
+++ run.sh
@@ -1,3 +1,7 @@
-sudo service docker start &&
+#!/bin/bash
+
+sudo service docker start
+source ./env/Scripts/activate
 docker-compose up -d	&&
-docker-compose run mypython pytest --alluredir=results tests_cases/test_suite_demo.py --browser docker-remote
+docker-compose run mypython pytest --alluredir=results tests_cases/test_suite_demo.py --browser docker-remote &&
+deactivate
